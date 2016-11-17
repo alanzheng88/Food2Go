810b64944f83f7687f7b00e456ecd5af4df88eb3
Franky Lu <ftlu@sfu.ca>
2016-11-16 16:00:27 -0800

    Adding in restaurant model and CRU method in application controller.
    Former-commit-id: 2fd14f71d6a101b794f3834bdc61e61d7609fcbf

--- api/app/controllers/Application.java
+++ api/app/controllers/Application.java
@@ -12,5 +12,34 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
-
+	
+	public static void restaurants(){
+		List<Restaurant> restaurantList = Restaurant.find("order by name").fetch();
+        renderJSON(restaurantList);
+	}
+	
+	public static void newRestaurant(@Required String name, @Required String owner, @Required String phoneNumber, String address){
+    	if (validation.hasErrors()) {
+    		flash.error("All fields with * are required!");
+        }
+    	addRestaurant(name, owner, phoneNumber, address);
+    }
+	
+	public static void editRestaurant(Long restaurantId, @Required String name, 
+    		@Required String owner, @Required String phoneNumber, String address){
+    	Restaurant restaurant = Restaurant.findById(restaurantId);
+    	if (validation.hasErrors()) {
+    		flash.error("All fields with * are required!");
+            renderJSON("Application/edit.html", restaurant);
+        }
+    	user.modifyUser(firstName, lastName, email, phoneNumber, notes);
+    	flash.success("User has been updated!");
+    	show(user.id);
+    }
+	
+	public static void addRestaurant(String name, String owner, String phoneNumber, String address){
+		Restaurant newRestaurant = new Restaurant(name, owner, phoneNumber, address).save();
+    	flash.success("New Restaurant Added!");
+    	restaurants();
+	}
 }
