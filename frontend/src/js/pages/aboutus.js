ba133c01f7071a4bd5b2aa4356594dd4cdf1edc9
Jean Wu <hcw8@sfu.ca>
2016-11-26 14:15:02 -0800

    add a simple page about us
    Former-commit-id: de1d5a1b4a4662800a3749f928fbe13195abc41a

---
+++ frontend/src/js/pages/aboutus.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class AboutUs extends React.Component {
+  render() {
+    console.log("AboutUs");
+    return (
+      <h1>About Us</h1>
+    );
+  }
+}
