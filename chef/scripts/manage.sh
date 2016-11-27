f06d0ba80f550fd0ff5c0128968bc8ec06dc6897
alanz <alanz@sfu.ca>
2016-11-27 15:23:31 -0800

    change vagrant file to use rsync and remove duplicate declaration of function usage
    Former-commit-id: a359c34b2ad4c6fa7702699cd64a789e5967e65c

--- chef/scripts/manage.sh
+++ chef/scripts/manage.sh
@@ -2,25 +2,10 @@
 
 set -o verbose
 
-function restartBackendServer {
-    echo "Restarting server"
-    sudo devscripts/clearTables.sh
-    sudo kill -9 $(sudo lsof -t -i:9000)
-    pushd api/
-    screen -d -m /home/downloads/play-1.4.3/play run --%dev
-    popd
-    sleep 5
-    echo "Warm up backend server"
-    curl -X GET -m 60 http://localhost:9000
-}
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
-function restartFrontendServer {
-    sudo kill -9 $(sudo lsof -t -i:12345)
-    pushd frontend/
-    npm install
-    screen -d -m /usr/bin/npm run dev
-    popd
-}
+. "$DIR/helper.sh"
 
 restartBackendServer
 restartFrontendServer
