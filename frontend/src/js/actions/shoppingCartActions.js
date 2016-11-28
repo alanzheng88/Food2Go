d2caa04e1ab02fd36f82ec4eabef2db27197e465
haifuy <haifuy@sfu.ca>
2016-11-27 17:30:03 -0800

    add basic shopperCart store/actions
    Former-commit-id: ea385466711380ede151359a659ba67abfaed14b

--- frontend/src/js/actions/shoppingCartActions.js
+++ frontend/src/js/actions/shoppingCartActions.js
@@ -2,56 +2,22 @@ import dispatcher from "../dispatcher";
 import axios from "axios";
 import {host, port} from "../constants/backend.js"
 
-export function authenticateUser(text) {
+export function getFoodList(text) {
   console.log("Sending the data!", text);
-  axios.post(`http://${host}:${port}/api/authenticate`, text).then((response) => {
+  axios.post(`http://${host}:${port}/api/shopping-cart@foodid=${text}`).then((response) => {
     dispatcher.dispatch({
-    	type: "AUTH_SUCCESS",
+    	type: "GET_SC_RESPONSE",
     	response
     });
-    console.log("got the response!", response);
+    console.log("Action: shoppingCart response!", response);
   })
   .catch((error) => {
-    console.log("Action: authenticateUser error", error);
+    console.log("Action: shoppingCart error", error);
   	dispatcher.dispatch({
-      	type: "AUTH_FAILURE",
+      	type: "GET_SC_ERROR",
       	error,
       });
   })
 }
 
-export function getUserInfo(sessionId) {
-	axios.get(`http://${host}:${port}/api/user?sessionid=${sessionId}`)
-    .then((response) => {
-      console.log("Action: getUserInfo response", response);
-      dispatcher.dispatch({
-      type: "UPDATE_USERINFO",
-      response,
-    });
-    })
-    .catch((error) => {
-      console.log("Action: getUserInfo error")
-      dispatcher.dispatch({
-        type: "UPDATE_USERINFO",
-        response : {
-          firstName: 'test',
-          lastName: 'test',
-          email: 'test@sfu.ca',
-          role: 'restaurantOwner',
-          restaurants: [],
-        }
-      });
-    })
-}
-
-export function logoutUser(sessionId) {
-	axios({
-    method: 'DELETE',
-    url: `http://${host}:${port}/api/authenticate?sessionid=${sessionId}`,
-    headers: {'Content-Type': 'application/json'}
-  }
-  ).then((response) => {
-	dispatcher.dispatch({type: "LOGOUT"});
-	console.log("got the response!", response);
-  })
 }
