032801cd97f6f417957d3e02bfde14850a7405e3
Jean Wu <hcw8@sfu.ca>
2016-11-28 22:29:35 -0800

    contactus page almost done #23
    Former-commit-id: 3e04dadc2b930e5a1a9566f01b4f110143120069

---
+++ frontend/src/js/pages/contactus.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Mailto from "react-mailto";
+export default class ContactUs extends React.Component {
+  render() {
+    console.log("ContactUs");
+    return (
+    <div class="row">
+    	<h1>Contact Us</h1>
+    	<div class="col-md-5">
+    		<h4>Join Food2Go</h4>
+    		<p>If you want to become one of us, and have html, react.js with babel, play and test experience. Please email us through <Mailto email="joinfood2go@sfu.ca" obfuscate={true}>joinfood2go@sfu.ca</Mailto></p>
+    	</div>
+    	<div class="col-md-2">
+    	</div>
+    	<div class="col-md-5">
+    		<h4>Contact us for specific problem</h4>    	</div>
+    </div>
+    );
+  }
+}
