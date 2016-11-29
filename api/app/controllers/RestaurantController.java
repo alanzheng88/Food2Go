723ba0345259b68958b3e4d1314cc28c1fd6caa5
Franky Lu <franky.lu@visioncritical.com>
2016-11-28 18:38:35 -0800

    adding routes and picture model #31
    Former-commit-id: a402065cf7404e2f1720c49f38942581b6376d0f

--- api/app/controllers/RestaurantController.java
+++ api/app/controllers/RestaurantController.java
@@ -3,8 +3,13 @@ package controllers;
 import play.*;
 
 import play.mvc.*;
+import play.data.Upload;
 
 import java.util.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 
 import models.*;
 import play.db.jpa.*;
@@ -38,4 +43,26 @@ public class RestaurantController extends AppController {
         response.status = 200;
     }
 
+    public static void uploadPicture(Upload data) {
+        Picture picture = new Picture();
+    	Logger.info(data.getContentType());
+    	Logger.info(data.getFieldName());
+    	Logger.info(data.getFileName());
+    	picture.contentType = data.getContentType();
+    	picture.fileName = data.getFileName();
+    	picture.file = data.asBytes();
+    	picture.save();
+    	Logger.info("saving id=%s", picture.id);
+    	index();
+    }
+	public static void index() {
+    	List images = Picture.all().fetch();
+        render(images);
+    }
+	public static void show(Long id) {
+    	Logger.info("loading id=%s", id);
+    	Picture picture = Picture.findById(id);
+    	response.setContentTypeIfNotSet(picture.contentType);
+		renderBinary(new ByteArrayInputStream(picture.file), picture.file.length);
+    }
 }
