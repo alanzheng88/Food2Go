deffe99dec0cc3d6c768c3ed493b989531ed8d43
Jean Wu <hcw8@sfu.ca>
2016-11-29 20:48:33 -0800

    add axios info in ordernow #32
    Former-commit-id: 2564e1d842e75da1999f9eeda07d77c0a63efd7e

--- frontend/src/js/pages/ordernow.js
+++ frontend/src/js/pages/ordernow.js
@@ -13,10 +13,42 @@ export default class OrderNow extends React.Component {
 				}]
 		}
 	}*/
+	searchUpdated (term) {
+	    this.setState({searchTerm: term})
+	}
+	getFoods(){
+	// Get foods info via Axios
+		var th = this;
+		axios.get('http://localhost:9000/api/restaurant/'+id +'/Foods')
+		.then(function(response) {
+			console.log(response);
+			th.setState({
+					foods: response.data
+			});
+		})
+		.catch(function(error) {
+			console.log('Failed to get restaurant info!');
+			if (error.response) {
+			// The request was made, but the server responded with a status code
+			// that falls out of the range of 2xx
+			console.log(error.response.data);
+			console.log(error.response.status);
+			console.log(error.response.headers);
+			} 
+			else {
+			// Something happened in setting up the request that triggered an Error
+			console.log('Error', error.message);
+			}
+		});
+	}
 	render() {
+	const filterdFoods = this.state.foods.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS));
     console.log("OrderNow");
     return (
-    	<h1>Food List of selected Restaurant</h1>
+    	<div>
+    		<h1>Food List of selected Restaurant</h1>
+
+    	</div>
     );
   }
 }
