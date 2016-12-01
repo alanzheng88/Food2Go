643ac03aa3447744873adc941cf0705b2abcd2bf
haifuy <haifuy@sfu.ca>
2016-12-01 13:39:07 -0800

    add USERINFO cookie
    Former-commit-id: 67932f3b9860188bcf560b7c03def71d8b6211f1

--- frontend/src/js/stores/userStore.js
+++ frontend/src/js/stores/userStore.js
@@ -9,27 +9,25 @@ class UserStore extends EventEmitter {
   constructor() {
     super()
     var sessionId = cookie.load('SESSIONID');
+    var userInfo = cookie.load('USERINFO');
+
     if (sessionId === undefined) {
       this.session = {
         sessionId:'',
         loginStatus:false,
-      }
+      };
+      this.userInfo = this.getGuestInfo();
     } else {
       this.session = {
         sessionId:sessionId,
         loginStatus:true,
       }
-    }
-    var userInfo = cookie.load('USERINFO');
-    if(userInfo === undefined) {
-      this.userInfo = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        role: 'guest',
-      };
-    } else {
-      this.userInfo = userInfo;
+      if(userInfo === undefined) {
+        this.userInfo = this.getGuestInfo();
+      } else {
+        this.userInfo = userInfo;
+      }
+
     }
     this.sessionInit();
   }
@@ -49,10 +47,7 @@ class UserStore extends EventEmitter {
       .then((response) => {
         console.log("found valid cookie", response)
         if (response.status == 200 ) {
-          this.session.loginStatus = true;
-          this.userInfo = response;
-          cookie.save('USERINFO',response, { path: '/' });
-          this.emit("auth_success", this.session.loginStatus);
+          this.emit("auth_success", true);
         } else {
           cookie.remove('SESSIONID', { path: '/' });
           cookie.remove('USERINFO', { path: '/' });
@@ -109,6 +104,7 @@ class UserStore extends EventEmitter {
       case "UPDATE_USERINFO": {
         console.log("Store: received UPDATE_USERINFO, data: ", action.response.data)
         this.userInfo = action.response.data;
+        cookie.save('USERINFO', this.userInfo, { path: '/' });
         this.emit("update_userinfo", this.userInfo);
         break;
       }
@@ -120,14 +116,15 @@ class UserStore extends EventEmitter {
       }
       case "UPDATE_USERINFO_ERROR": {
         console.log("Store: received UPDATE_USERINFO, data: ")
-        this.session.loginStatus = false;        
+        this.session.loginStatus = false;
         this.userInfo = this.getGuestInfo();
         this.emit("logout", false);        
         this.emit("update_userinfo", this.userInfo);
         break;
       }
       case "LOGOUT": {
-        // cookie.remove('SESSIONID', { path: '/' });
+        cookie.remove('SESSIONID', { path: '/' });
+        cookie.remove('USERINFO', { path: '/' });
         this.session.loginStatus = false;
         this.userInfo = this.getGuestInfo();
         this.emit("logout", false);
