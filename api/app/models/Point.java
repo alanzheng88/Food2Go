914aecea0c963d03ea58cb1b33501ff4072419e9
Franky Lu <franky.lu@visioncritical.com>
2016-12-02 20:44:45 -0800

    re-adding controller, model and route #36
    Former-commit-id: cfc34adb2283c4216b03e66989da517cd99574ff

---
+++ api/app/models/Point.java
@@ -0,0 +1,21 @@
+package models;
+ 
+import java.util.*;
+import javax.persistence.*;
+ 
+import play.db.jpa.*;
+
+import play.data.validation.*;
+ 
+@Entity
+public class Point extends Model {
+ 
+    @Required
+    public int points;
+
+    public Point(int points) 
+	{
+        this.points = points;
+    }
+ 
+}
